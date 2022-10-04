50a33ea76a4417152af8ab682df2fb2fe77237e3
zoa0945 <zoa0945@gmail.com>
2022-10-04 23:35:05 +0900

    init: delete storyboard and make MainViewController

---
+++ FullScreenCamera/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  FullScreenCamera
+//
+//  Created by zoa0945 on 2022/10/04.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+    }
+}
